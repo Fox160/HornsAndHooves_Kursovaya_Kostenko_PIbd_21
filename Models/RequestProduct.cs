2bba7500f1547b6de9df470abc94535cff950453
Fox160 <ksenija-kstenk@mail.ru>
2018-03-25 16:18:33 +0400

    Реализация модуля работы с БД

---
+++ Models/RequestProduct.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class RequestProduct
+    {
+        public int Id { get; set; }
+
+        public int Count { get; set; }
+
+        public int ProductId { get; set; }
+
+        public int RequestId { get; set; }
+
+        public virtual Product Product { get; set; }
+
+        public virtual Request Request { get; set; }
+    }
+}
